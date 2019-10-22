fc14f5921761323955dcea78e8c683e431b39a46
alva <861729091@qq.com>
2019-10-22 11:06:54 +0800

    添加slot绑定事件

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -2,9 +2,14 @@
   <div class="setting">
     <!-- 遍历children列表 -->
     <template v-for="(child, index) in children">
-      <Child :childInfo="child" :key="index" />
+      <Child :childInfo="child" @EditProfile="OnEditProfile" :key="index">
+        <template v-slot:edit="childProps">
+          <div class="edit-profile">
+            <p class="text-blue" @click="showProfile=true;childIndex = index">修改资料></p>
+          </div>
+        </template>
+      </Child>
     </template>
-
     <!-- 添加孩子按钮 -->
     <div class="add-child">
       <div class="add-icon" @click="OnAddChild"></div>
@@ -15,7 +20,7 @@
 
     <!-- overlay提示弹窗 -->
     <div v-show="showDilog" @click="showDilog = false;">
-      <Overlay style="height: 100vh;">
+      <Overlay>
         <template v-slot:content="childProps">
           <div class="inner">
             <p>请用微信扫描新设备的二维码，添加新的用户</p>
@@ -23,6 +28,13 @@
         </template>
       </Overlay>
     </div>
+    <!-- 显示修改资料 -->
+    <Profile
+      :OnConfirmProfile="OnEditProfile"
+      :childInfo="selectedChild"
+      :OnClickOverlay="()=>{showProfile=false}"
+      v-show="showProfile"
+    />
   </div>
 </template>
 
@@ -30,22 +42,51 @@
 import Child from './layout/Child';
 import { mapState } from 'vuex';
 import Overlay from './layout/Overlay';
+import Profile from './layout/Profile';
+import types from '../store/types';
 export default {
   name: 'Setting',
-  components: { Child, Overlay },
+  components: { Child, Overlay, Profile },
   data() {
     return {
-      showDilog: false
+      showDilog: false,
+      showProfile: false,
+      childIndex: 0
     }
   },
   computed: {
     // 获取孩子列表
-    ...mapState(['children'])
+    children: {
+      get() {
+        return this.$store.state.children;
+      },
+      set(children) {
+        this.$store.commit(types.SET_CHILDREN, children);
+      }
+    },
+    selectedChild() {
+      return this.children[this.childIndex] || {};
+    }
   },
   methods: {
     OnAddChild() {
       console.log('OnAddChild');
       this.showDilog = true;
+    },
+    OnEditProfile({ avatar, name, grade, term }) {
+      const { watchId } = this.selectedChild;
+      const { children } = this;
+      console.log({ avatar, name, grade, term, watchId, children });
+      for (let i = 0; i < children.length; ++i) {
+        if (children[i].watchId == watchId) {
+          children[i].img = avatar;
+          children[i].nickName = name;
+          children[i].grade = grade;
+          children[i].terme = term;
+          localStorage.setItem('children', JSON.stringify(children));
+        }
+      }
+      this.showProfile = false;
     }
   }
 }
@@ -99,5 +140,12 @@ export default {
       transform: translateX(-50%) translateY(-50%) rotate(90deg);
     }
   }
+}
+
+.edit-profile {
+  text-align: right;
+  position: absolute;
+  right: 15px;
+  font-size: 14px;
 }
 </style>
